a0984e708e3a9c67053e395e03bc24d16b9331bb
Faisal Samudra <isalsamudra@hotmail.com>
2023-04-04 04:07:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ mongoose
 
 // connect to AWS redis
 const redisClient = redis.createClient({
-  url: `redis://${process.env.REDIS_URL}`
+  url: `redis://${process.env.REDIS_URL}`,
 });
 
 (async () => {
@@ -81,23 +81,26 @@ redisClient.on("connect", function (err) {
   console.log("==========Connected to redis successfully==========");
   // console.log("check if promo exists in redis....");
   redisClient.get("promo", (err, reply) => {
-    if (err) throw err;
-    if (reply != null) {
-      console.log("Promos already exists in redis");
+    if (err) {
+      console.log(`Error getting promo from Redis: ${err}`);
     } else {
-      console.log("Promos not exists in redis");
-      console.log("Setting promos to redis...");
-      redisClient.set(
-        "promo",
-        "Get 10% off your first purchase when you sign up for our newsletter!",
-        (err, reply) => {
-          if (err) {
-            console.log(`Error setting promo in Redis: ${err}`);
-          } else {
-            console.log("Promo set in Redis successfully!");
+      if (reply != null) {
+        console.log("Promos already exists in redis");
+      } else {
+        console.log("Promos not exists in redis");
+        console.log("Setting promos to redis...");
+        redisClient.set(
+          "promo",
+          "Get 10% off your first purchase when you sign up for our newsletter!",
+          (err, reply) => {
+            if (err) {
+              console.log(`Error setting promo in Redis: ${err}`);
+            } else {
+              console.log("Promo set in Redis successfully!");
+            }
           }
-        }
-      );
+        );
+      }
     }
   });
 });
